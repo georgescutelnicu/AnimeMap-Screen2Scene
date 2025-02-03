ffdc5e6e89702323d107a56222428085be1c015d
georgescutelnicu <george.v.scutelnicu@gmail.com>
2025-02-03 21:11:59 +0200

    Finish show anime/location feature

--- static/js/app.js
+++ static/js/app.js
@@ -36,8 +36,8 @@ updateCarousel();
 document.addEventListener("DOMContentLoaded", function () {
   var map = L.map('map').setView([35.6895, 139.6917], 5); // Tokyo center
 
-  L.tileLayer('https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
-    attribution: '&copy; Google Maps',
+  L.tileLayer('https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}&hl=en', {
+  attribution: '&copy; Google Maps',
   }).addTo(map);
 
   var customIcon = L.icon({
@@ -48,7 +48,7 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
 var locations = [];
-animeLocations.forEach(function(location) {
+mapPins.forEach(function(location) {
     var coords = location.coordinates.split(', ').map(Number);
 
     locations.push({
@@ -61,11 +61,104 @@ animeLocations.forEach(function(location) {
     L.marker(location.coords, { icon: customIcon })
       .addTo(map)
       .bindPopup(`
-        <b><button class="mapButton" onclick="flipMap(${index})">${location.name}</button></b>
+        <b><button class="mapButton" onclick="showLocation('${location.name}')">${location.name}</button></b>
       `);
   });
 });
 
-function flipMap(index) {
-  alert(`Flip map function triggered for location: ${index}`);
+function showAnime(animeName) {
+    const anime = animeDetails[animeName];
+    let html = `<div class="location-container">`;
+
+    html += `<div class="title">${anime.english_name}
+    ${anime.romaji_name.toLowerCase() != anime.english_name.toLowerCase() ? ' - ' + anime.romaji_name : ''}</div>`;
+
+    html += `
+        <p class="info">
+            <span class="anime-type">${anime.anime_type}</span> |
+            <span class="year">${anime.year_of_release}</span> |
+            <span class="episodes">${anime.number_of_episodes} episodes</span>
+        </p>
+    `;
+
+    html += `<p class="description">${anime.description.replace(/\s*\[Written by MAL Rewrite\]$/, '')}</p>`;
+
+    html += `<div class="locations">`;
+    for (let locationKey in anime.locations) {
+        const location = anime.locations[locationKey];
+
+        html += `<div class="location">`;
+        html += `<h4 class="location-name">${location.location_name}</h4>`;
+
+        let animePhotos = JSON.parse(location.anime_photos.replace(/'/g, '"'));
+        let realPhotos = JSON.parse(location.real_photos.replace(/'/g, '"'));
+
+        html += `<div class="photos">`;
+        for (let i = 0; i < animePhotos.length; i++) {
+            html += `
+                <div class="flip-container">
+                    <div class="flipper">
+                        <div class="front">
+                            <img class="photo" src="${animePhotos[i]}" alt="Anime Photo ${i+1}" />
+                        </div>
+                        <div class="back">
+                            <img class="photo" src="${realPhotos[i]}" alt="Real Photo ${i+1}" />
+                        </div>
+                    </div>
+                </div>
+            `;
+        }
+        html += `</div>`;
+        html += `</div>`;
+    }
+    html += `</div>`;
+
+    html += `</div>`;
+
+    document.getElementById('infoContent').innerHTML = html;
+}
+
+function showLocation(locationName) {
+    const locations = animeLocations[locationName];
+
+    let html = `<div class="location-container">`;
+    html += `<div class="title">${locations[0].location_name}</div>`;
+
+    locations.forEach(loc => {
+        const details = loc.anime_details;
+
+        html += `<h3 class="subtitle">${details.english_name}${
+            details.english_name.toLowerCase() !== details.romaji_name.toLowerCase()
+                ? ` <span class="subtitle">- ${details.romaji_name}</span>`
+                : ""
+        }</h3>`;
+
+        let animePhotos = [];
+        let realPhotos = [];
+
+        animePhotos = JSON.parse(loc.anime_photos.replace(/'/g, '"'));
+        realPhotos = JSON.parse(loc.real_photos.replace(/'/g, '"'));
+
+        html += `<div class="photos">`;
+        for (let i = 0; i < animePhotos.length; i++) {
+            html += `
+                <div class="flip-container">
+                    <div class="flipper">
+                        <div class="front">
+                            <img class="photo" src="${animePhotos[i]}" alt="Anime Photo ${i+1}" />
+                        </div>
+                        <div class="back">
+                            <img class="photo" src="${realPhotos[i]}" alt="Real Photo ${i+1}" />
+                        </div>
+                    </div>
+                </div>
+            `;
+        }
+        html += `</div>`;
+
+    });
+
+    html += `</div>`;
+
+    document.getElementById('infoContent').innerHTML = html;
 }
